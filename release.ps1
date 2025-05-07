666236f730bd5730da49ab1efb971527310ac2df
Ali Muhammad <ali.munchkin08@gmail.com>
2025-05-07 16:24:08 +0500

    initial commit

---
+++ release.ps1
@@ -0,0 +1,2 @@
+dotnet publish Flow.Launcher.Plugin.VirtualDesktop -c Release -r win-x64 --no-self-contained
+Compress-Archive -LiteralPath Flow.Launcher.Plugin.VirtualDesktop/bin/Release/win-x64/publish -DestinationPath Flow.Launcher.Plugin.VirtualDesktop/bin/VirtualDesktop.zip -Force
